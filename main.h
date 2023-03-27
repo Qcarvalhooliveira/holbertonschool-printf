5c0ee2c2c7c97e72b2eeac62bb66053d2e7bf620
root <chouikhi_aicha@live.fr>
2023-03-27 03:27:59 -0700

    printf main.h

---
+++ main.h
@@ -0,0 +1,23 @@
+#ifndef _MAIN_H_
+#define _MAIN_ H_
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdstring.h>
+#include <limits.h>
+#include <unistd.h>
+/**
+   *struct func_type - type of structure.
+   * @t: pointer to argument.
+   * @f: pointer - function associated with argument.
+ */
+typedef struct func_type
+{
+       char *t;
+       int (*f) (va_list);
+} func_t;
+int (*get_func(*get_func(const char *format))(va_list);
+int _putchar (char c);
+int _printf(const char *format, ...);
+
+#endif
