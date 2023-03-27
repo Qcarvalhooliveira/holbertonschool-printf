6f66330ad62407a44ff40060cc2235c74ba1da4f
root <queisecarvalho@hotmail.com>
2023-03-27 05:32:41 -0700

    Printf task 0

---
+++ _printf.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _printf - function that produces output according to a format.
+ * @format: character string.
+ *
+ * Return: the number of characters printed.
+ */
+
+int _printf(const char *format, ...)
+{
+	int = 0;
+	str = 0;
+	int (*f)(va_list);
+	va_list args;
+
+	
